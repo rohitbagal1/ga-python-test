2d00447b2c6b7702c140f7d417f1b6d07c249cc2
rohitbagal1 <90562891+rohitbagal1@users.noreply.github.com>
2024-08-08 13:03:40 +0530

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,1 +1,8 @@
+import unittest
+from src.main import add
+class TestMain(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(1, 2), 3)
+if __name__ == "__main__":
+    unittest.main()
 
